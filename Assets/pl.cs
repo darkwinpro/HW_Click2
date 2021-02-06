e55564d677c2b346de9006b2ecc3329793ca9f97
Darkwin <Darkwin.pro@gmail.com>
2021-02-06 18:44:31 +0300

    new version

---
+++ Assets/pl.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pl : MonoBehaviour
+{
+    public LayerMask toClickOn;
+    public AudioSource clicked;
+    private UnityEngine.AI.NavMeshAgent myAgent;
+    void Start()
+    {
+        myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(myRay, out hit, 100, toClickOn))
+            {
+                myAgent.SetDestination(hit.point);
+                
+            }
+            Debug.Log("Come Here!");
+            clicked.Play();
+        }
+    }
+}
